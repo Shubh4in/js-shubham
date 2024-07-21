797956885efa9b7a3d8de59445a44dcb08b0ee40
SHUBHAM KUMAR <144233309+Shubh4in@users.noreply.github.com>
2024-07-21 21:43:52 +0530

    Add files via upload

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,11 @@
+const accountId= 101
+let accountEmail="Shubhamshreyash360@gmail.com"
+var accountName ="Shubham"
+accountCity="purnia"
+let accountState;
+
+accountEmail="Shubhamshreyash180@gmail.com"
+accountName ="Shubham kumar"
+accountCity="Hyderabad"
+console.table([accountId,accountName,accountEmail,accountCity,accountState])
+
